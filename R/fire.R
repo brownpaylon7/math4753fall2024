4ec691e1c978502cf775a57853fdf9485ec55a2b
Paylon-Brown <paylon.g.brown-1@ou.edu>
2024-10-18 22:18:51 -0500

    Honestly I forgot

---
+++ R/fire.R
@@ -0,0 +1,13 @@
+#' Mendenhall-Sincich data
+#'
+#' A subset of data from the Statistics for Engineering and the Sciences book about fires and their damages
+#' relative to their distance from the nearest fire station
+#'
+#' @format A data frame with 15 rows and 2 variables:
+#' \describe{
+#'   \item{DISTANCE}{distance from fire station in miles}
+#'   \item{DAMAGE}{X 1000 dollars in damage}
+#' }
+#' @source \url{https://www.crcpress.com/Statistics-for-Engineering-and-the-Sciences/Mendenhall-Sincich/p/book/9781498728850}
+#'
+"fire"
